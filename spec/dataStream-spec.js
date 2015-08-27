f3b2e06818f2adec50392019a24efef4858302d6
Michel Hoang <hoang_m@epitech.eu>
2015-08-28 00:52:35 +0200

    michel testunit js

--- spec/dataStream-spec.js
+++ spec/dataStream-spec.js
@@ -90,38 +90,90 @@ function executeDataStreamTest(dataStreamName) {
     describe('length', function () {
       it('should return length', function () {
         expect(dataStream.length()).toEqual(jasmine.any
-        (Peeracle.MemoryDataStream.options.buffer.length));
+        (Peeracle.MemoryDataStream.buffer.length));
       });
     });
     describe('tell', function () {
-      it('should return 0', function () {
+      it('should return offset', function () {
         expect(dataStream.tell()).toEqual(0);
       });
     });
 
     describe('seek', function () {
-      var position;
-        it("should be number", function() {
-          if (typeof position !== 'number') {
-            expect(dataStream.seek()).toThrow(jasmine.any(TypeError));
-          }
-        });
-        it("should be between 0 and buffer length", function() {
-          if (position < 0 || position > Peeracle.MemoryDataStream.buffer.length) {
-            expect(dataStream.seek()).toThrow(jasmine.any(RangeError));
-          }
-        });
-      it("should return position", function() {
-        expect((Peeracle.MemoryDataStream.offset).toEqual(position));
-        expect(dataStream.seek()).toEqual(position);
+      it('should call seek with an object and throw an type error',
+        function () {
+        expect(function () {
+          dataStream.seek('a');
+        }).toThrow(jasmine.any(TypeError));
+      });
+      it('should call seek with an object and throw an range error',
+        function () {
+        expect(function () {
+          dataStream.seek(-1);
+        }).toThrow(jasmine.any(RangeError));
+          expect(function () {
+            dataStream.seek(6);
+          }).toThrow(jasmine.any(RangeError));
       });
     });
 
     describe('read', function () {
+      it('should call read with two objects',
+        function () {
+          var callback = jasmine.createSpy('callback');
+
+          expect(dataStream.read(-1, callback)).toThrow(jasmine.any(RangeError));
+        });
+      it('should call read',
+        function () {
+          expect(dataStream.read({})).toHaveBeenCalled();
+        });
     });
+
+    describe('readChar', function () {
+      it('should call readChar with two objects and throw error',
+        function () {
+          var callback = jasmine.createSpy('callback');
+
+          expect(dataStream.readChar(callback)).toThrow(jasmine.any(RangeError));
+        });
+    });
+
     describe('peek', function () {
+      it('should call peek with two objects and throw error',
+        function () {
+          var callback = jasmine.createSpy('callback');
+
+          expect(dataStream.peek(-1, callback)).toThrow(jasmine.any(RangeError));
+
+        });
+      it('should call peek',
+        function () {
+          expect(dataStream.peek({})).toHaveBeenCalled();
+        });
     });
+
+    describe('peekChar', function () {
+      it('should call peekChar with callback',
+        function () {
+          var callback = jasmine.createSpy('callback');
+          expect(dataStream.peekChar(callback)).toHaveBeenCalled();
+        });
+    });
+
     describe('write', function () {
+      it('should call write with two objects and throw type error',
+        function () {
+          var callback = jasmine.createSpy('callback');
+
+          expect(dataStream.write('a', callback)).toThrow(jasmine.any(TypeError));
+        });
+      it('should call write with two objects and throw range error',
+        function () {
+          var callback = jasmine.createSpy('callback');
+
+          expect(dataStream.write(new Uint8Array(1), callback)).toThrow(jasmine.any(RangeError));
+        });
     });
   });
 }
